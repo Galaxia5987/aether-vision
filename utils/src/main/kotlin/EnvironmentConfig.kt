172f0d08a6956e6adcd55a3fb3d94e5a3e21292b
AdarWa <adar.wasserman@gmail.com>
2026-08-13 21:28:13 +0300

    Add warning when in DEV mode

--- utils/src/main/kotlin/EnvironmentConfig.kt
+++ utils/src/main/kotlin/EnvironmentConfig.kt
@@ -1,13 +1,21 @@
+import org.slf4j.LoggerFactory
+
 enum class Environment {
     DEV,
     PROD,
 }
 
 object EnvironmentConfig {
+    private val logger = LoggerFactory.getLogger(this::class.simpleName)
+
     val CURRENT_ENVIRONMENT: Environment =
         when (System.getenv("DEV")) {
             null -> Environment.PROD
             else -> Environment.DEV
+        }.also {
+            if(it == Environment.DEV){
+                logger.info("Development mode enabled!")
+            }
         }
 
     val HTTP_PORT: Int = (System.getenv("HTTP_PORT") ?: "5803").toInt()
