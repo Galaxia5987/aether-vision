ced345a59bef0ec617757c8f18044ce4f9180606
AdarWa <adar.wasserman@gmail.com>
2026-08-14 21:10:57 +0300

    Create basic null broker

--- app/src/main/kotlin/App.kt
+++ app/src/main/kotlin/App.kt
@@ -2,6 +2,7 @@ package com.galaxia5987.app
 
 import com.galaxia5987.NativeLibrary
 import com.galaxia5987.server.Server
+import com.galaxia5987.server.streaming.broker.StreamingBrokers
 import logging.configureLogger
 
 fun main() {
@@ -9,5 +10,6 @@ fun main() {
         println(it.add(200, 2))
     }
     configureLogger()
+    StreamingBrokers.addBroker("color") { null }
     Server.start()
 }
