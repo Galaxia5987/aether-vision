3c40aa9498e81de20f104a5bf06ee3d21df27670
AdarWa <adar.wasserman@gmail.com>
2026-08-14 18:01:21 +0300

    Make Configurator.svelte work with multiple input types

--- frontend/src/configurations/Configurator.svelte
+++ frontend/src/configurations/Configurator.svelte
@@ -1,34 +1,51 @@
-<script lang="ts" module>
-    import { Pane, Folder, Text, Button, Slider, Checkbox, List, type ListOptions} from 'svelte-tweakpane-ui';
+<script lang="ts">
+    import { onMount } from 'svelte';
+    import { Pane, Folder, Text, Button, Slider, Checkbox, List, type ListOptions } from 'svelte-tweakpane-ui';
     import { config as configPkg } from 'utils';
+
     type AppConfig = configPkg.structs.AppConfig;
     const ConfigClient = configPkg.ConfigClient;
 
-    const inputOptions: ListOptions<string> = {};
-    const inputDefaults = configPkg.structs.InputConfig.Companion.getOptions().asJsReadonlyMapView();
-    inputDefaults.forEach((value: configPkg.structs.InputConfig, key: string, map: ReadonlyMap<string, configPkg.structs.InputConfig>) => {
-        inputOptions[key] = key
-    });
-
-    function handleSelectionChange(selection: string) {
-        config.input = inputDefaults[selection]
-    }
-
-    let inputSelection: string = inputOptions[0]
-    $: handleSelectionChange(inputSelection);
-
-
     const client = new ConfigClient(window.location.origin);
     let config = configPkg.structs.defaultInstance();
-    try {
-        config = await client.fetchConfig();
-    }catch (e){
-        console.error(e)
+
+    // Fetch configuration after component mount to prevent blocking rendering
+    onMount(async () => {
+        try {
+            config = await client.fetchConfig();
+        } catch (e) {
+            console.error(e);
+        }
+    });
+
+    async function save() {
+        await client.updateConfig(config);
     }
 
-    async function save(){
-        await client.updateConfig(config)
+    const inputOptions: Record<string, string> = {};
+    const inputDefaults = configPkg.structs.InputConfig.Companion.getOptions().asJsReadonlyMapView();
+
+    inputDefaults.forEach((value: configPkg.structs.InputConfig, key: string) => {
+        inputOptions[key] = key;
+    });
+
+    let inputSelection: string;
+    if(config.input instanceof configPkg.structs.UsbCamera){
+        inputSelection = "UsbCamera"
+    }else if(config.input instanceof configPkg.structs.RealsenseCamera){
+        inputSelection = "RealsenseCamera"
     }
+
+    function handleSelectionChange(selection: string) {
+        if (!selection) return;
+
+        const selectedInput = inputDefaults.get(selection);
+        if (selectedInput) {
+            config.input = selectedInput;
+        }
+    }
+
+    $: handleSelectionChange(inputSelection);
 </script>
 
 <Pane title="Configuration" localStoreId="config">
@@ -36,22 +53,23 @@
         <List bind:value={inputSelection} label="Type" options={inputOptions} />
         {#if config.input instanceof configPkg.structs.UsbCamera}
             <Folder title="Resolution">
-                <Slider bind:value={config.input.resolution.width} label="Width"/>
-                <Slider bind:value={config.input.resolution.height} label="Height"/>
+                <Slider bind:value={config.input.resolution.width} label="Width" />
+                <Slider bind:value={config.input.resolution.height} label="Height" />
             </Folder>
             <Folder title="Color Stream">
-                <Checkbox bind:value={config.input.colorStream.enabled} label="Enable"/>
+                <Checkbox bind:value={config.input.colorStream.enabled} label="Enable" />
             </Folder>
         {:else if config.input instanceof configPkg.structs.RealsenseCamera}
             <Folder title="Resolution">
-                <Slider bind:value={config.input.resolution.width} label="Width"/>
-                <Slider bind:value={config.input.resolution.height} label="Height"/>
+                <Slider bind:value={config.input.resolution.width} label="Width" />
+                <Slider bind:value={config.input.resolution.height} label="Height" />
             </Folder>
+            <Slider bind:value={config.input.fps} label="FPS"/>
             <Folder title="Color Stream">
-                <Checkbox bind:value={config.input.colorStream.enabled} label="Enable"/>
+                <Checkbox bind:value={config.input.colorStream.enabled} label="Enable" />
             </Folder>
             <Folder title="Depth Stream">
-                <Checkbox bind:value={config.input.depthStream.enabled} label="Enable"/>
+                <Checkbox bind:value={config.input.depthStream.enabled} label="Enable" />
             </Folder>
         {/if}
     </Folder>
