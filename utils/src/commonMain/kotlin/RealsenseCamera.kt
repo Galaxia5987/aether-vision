5976ec274976cdb28ab963889743c8d32ceeef4c
AdarWa <adar.wasserman@gmail.com>
2026-08-14 15:57:35 +0300

    Set serial names

--- utils/src/commonMain/kotlin/RealsenseCamera.kt
+++ utils/src/commonMain/kotlin/RealsenseCamera.kt
@@ -1,15 +1,28 @@
 package config.structs
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import kotlin.js.ExperimentalJsExport
 import kotlin.js.JsExport
 
 @OptIn(ExperimentalJsExport::class)
 @JsExport
+@SerialName("RealsenseCamera")
 @Serializable
 data class RealsenseCamera(
     val resolution: Resolution,
     val fps: Int,
     val colorStream: StreamConfig,
     val depthStream: StreamConfig,
-) : InputConfig()
+) : InputConfig(), DefaultValue<RealsenseCamera> {
+    override fun defaultInstance(): RealsenseCamera =
+        RealsenseCamera(
+            Resolution(
+                640,
+                480
+            ),
+            30,
+            colorStream = StreamConfig(enabled = true),
+            depthStream = StreamConfig(enabled = true)
+        )
+}
--- utils/src/commonMain/kotlin/UsbCamera.kt
+++ utils/src/commonMain/kotlin/UsbCamera.kt
@@ -1,13 +1,24 @@
 package config.structs
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import kotlin.js.ExperimentalJsExport
 import kotlin.js.JsExport
 
 @OptIn(ExperimentalJsExport::class)
 @JsExport
+@SerialName("UsbCamera")
 @Serializable
 data class UsbCamera(
     val resolution: Resolution,
     val colorStream: StreamConfig
-) : InputConfig()
+) : InputConfig(), DefaultValue<UsbCamera> {
+    override fun defaultInstance(): UsbCamera =
+        UsbCamera(
+            Resolution(
+                640,
+                480
+            ),
+            colorStream = StreamConfig(enabled = true),
+        )
+}
