cffc960e143207f88070c58b20e7372b5b01194d
AdarWa <adar.wasserman@gmail.com>
2026-08-14 21:10:05 +0300

    Add EmptyFramePusher

---
+++ server/src/main/kotlin/streaming/broker/EmptyFramePusher.kt
@@ -0,0 +1,18 @@
+@file:Suppress("SameParameterValue")
+
+package com.galaxia5987.server.streaming.broker
+
+
+private object ResourceHelper {
+    private fun loadJpegFromResources(fileName: String): ByteArray {
+        val inputStream = javaClass.getResourceAsStream("/$fileName") ?: error("Resource file not found: $fileName")
+
+        return inputStream.use { it.readBytes() }
+    }
+
+    val emptyFrameImage = loadJpegFromResources("emptyFrame.jpeg")
+}
+
+class EmptyFramePusher : StreamPusher {
+    override fun acceptFrame(): ByteArray = ResourceHelper.emptyFrameImage
+}
