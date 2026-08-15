141ab49c386197d768c1f52dc9e6740eb8207bcb
AdarWa <adar.wasserman@gmail.com>
2026-08-15 18:47:16 +0300

    Modify App.kt to use UsbCamera

--- app/src/main/kotlin/App.kt
+++ app/src/main/kotlin/App.kt
@@ -1,15 +1,17 @@
 package com.galaxia5987.app
 
-import com.galaxia5987.NativeLibrary
+import com.galaxia5987.app.camera.StreamType
+import com.galaxia5987.app.camera.usb_camera.UsbCamera
 import com.galaxia5987.server.Server
 import com.galaxia5987.server.streaming.broker.StreamingBrokers
+import config.Config
+import config.structs.UsbCameraConfig
 import logging.configureLogger
 
 fun main() {
-    NativeLibrary().use {
-        println(it.add(200, 2))
-    }
     configureLogger()
-    StreamingBrokers.addBroker("color") { null }
+    val cam = UsbCamera()
+    cam.start(Config.load().input as UsbCameraConfig)
+    StreamingBrokers.addBroker("color", cam.makeStreamPusher(StreamType.COLOR))
     Server.start()
 }
